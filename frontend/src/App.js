d4d097af22f02767899e2c67b2d91b1d9b5d4f30
melonattacker <>
2020-08-17 14:49:27 +0900

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App;
