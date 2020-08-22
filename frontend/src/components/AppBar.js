8f16aa264c6c84133f0ec601f7fe09029e894dcc
melonattacker <>
2020-08-23 02:28:21 +0900

    set the link to home

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +28,9 @@ export default function ButtonAppBar() {
       <AppBar className={classes.bar} position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Utility Token Price Simulator
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+              Utility Token Price Simulator
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
